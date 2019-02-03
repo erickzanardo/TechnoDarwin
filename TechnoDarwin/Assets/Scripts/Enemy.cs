bddaaea00df7f7b8ae0b6cc3645131c73ef1d76e
Briag Dupont <briagd@yahoo.com>
2019-02-02 23:57:42 -0600

    started

---
+++ TechnoDarwin/Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public float speed;
+    public float noiseInc;
+    
+
+    private Animator animator;
+    public GameObject bloodEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health<=0){
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int dam){
+        Instantiate(bloodEffect, transform.position, Quaternion.identity);
+        health -= dam;
+        Debug.Log("Damage taken");
+    }
+
+}
---
+++ TechnoDarwin/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    Rigidbody2D rb;
+    public Animator animator;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+   
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        animator.SetFloat("speed", Mathf.Abs(moveHorizontal) + Mathf.Abs(moveVertical));
+        rb.transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
